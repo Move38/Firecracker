0635cdfd693030ab2a75baf79c79002a242a05f7
jbobrow <jbobrow@gmail.com>
2017-12-31 14:43:44 -0500

    Description of firecracker algo

---
+++ Firecracker.ino
@@ -0,0 +1,27 @@
+/*
+ *  Happy New Years - 2018
+ *  
+ *  Firecracker
+ *  
+ *  Rules:
+ *  If button pressed...
+ *  explode with firecracker like flashing of lights
+ *  when done, pick one of my neighbors to send the spark to
+ *  if receive spark, explode like firecracker
+ *  remember who I received it from
+ *  when done, pick one of my other neighbors to send the spark to
+ *  if no other neighbors available, firecracker is extinguished
+ *  
+ */
+
+#include "blinklib.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
