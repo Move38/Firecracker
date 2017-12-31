3228f2778a0c2ecf10de0cd5824308680d1abdf8
jbobrow <jbobrow@gmail.com>
2017-12-31 14:50:52 -0500

    show neighbors recognize each other

--- Firecracker.ino
+++ Firecracker.ino
@@ -1,18 +1,18 @@
 /*
- *  Happy New Years - 2018
- *  
- *  Firecracker
- *  
- *  Rules:
- *  If button pressed...
- *  explode with firecracker like flashing of lights
- *  when done, pick one of my neighbors to send the spark to
- *  if receive spark, explode like firecracker
- *  remember who I received it from
- *  when done, pick one of my other neighbors to send the spark to
- *  if no other neighbors available, firecracker is extinguished
- *  
- */
+    Happy New Years - 2018
+
+    Firecracker
+
+    Rules:
+    If button pressed...
+    explode with firecracker like flashing of lights
+    when done, pick one of my neighbors to send the spark to
+    if receive spark, explode like firecracker
+    remember who I received it from
+    when done, pick one of my other neighbors to send the spark to
+    if no other neighbors available, firecracker is extinguished
+
+*/
 
 #include "blinklib.h"
 
@@ -23,5 +23,19 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // let neighbors know I am here
+  irBroadcastData(1);
+
+  // read my neighbors
+  FOREACH_FACE(f) {
+    if(irIsReadyOnFace(f)){
+      irGetData(f);
+      setFaceColor(f, RED);
+    }
+    else {
+      setFaceColor(f, OFF);
+    }
+  }
 
 }
